8988234db750a349ea2ed8080c5f2827448428b2
Konstantin Stute <konstantin.stute@stud.tu-darmstadt.de>
2024-08-30 19:00:25 +0200

    added launch structure

---
+++ launch/vrs_project.launch.py
@@ -0,0 +1,42 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+epuck_name = 'epuck1' #adjust this per epuck
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package="epuck_driver_cpp_ros2",
+            executable="epuck_driver_cpp_ros2_node",
+            name = "epuck_driver_cpp_ros2_node",
+            parameters=[
+                {'epuck_name': epuck_name} # add more parameters here if needed
+                        ]
+        ),
+        Node(
+            package="epuck_movement_controller",
+            executable="epuck_movement_controller_node",
+            name = "epuck_movement_controller_node",
+            parameters=[
+                {'epuck_name': epuck_name} # add more parameters here if needed
+                        ]
+        ),
+        Node(
+            package="image_centering",
+            executable="image_centering_node",
+            name = "image_centering_node",
+            parameters=[
+                {'epuck_name': epuck_name} # add more parameters here if needed
+                        ]
+        ),
+        Node(
+            package="epuck_tof_ros2",
+            executable="VL53L0X_node",
+            name = "epuck_tof_ros2_node",
+            parameters=[
+                {'epuck_name': epuck_name} # add more parameters here if needed
+                        ]
+        )
+    ])
